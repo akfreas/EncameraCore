aa6cdf86ff203c67deacb8a45002a84f97fab66c
Alexander Freas <akfreas@gmail.com>
2023-12-21 15:30:24 +0100

    Add debugprintable to help with debugging

---
+++ Sources/EncameraCore/Extensions/DebugPrintable.swift
@@ -0,0 +1,20 @@
+//
+//  DebugPrintable.swift
+//
+//
+//  Created by Alexander Freas on 21.12.23.
+//
+
+import Foundation
+public protocol DebugPrintable {
+
+}
+
+extension DebugPrintable {
+    public func printDebug(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+        let className = String(describing: type(of: self))
+        let message = items.map { "\($0)" }.joined(separator: separator)
+        print("\(className): \(message)", terminator: terminator)
+    }
+}
+
