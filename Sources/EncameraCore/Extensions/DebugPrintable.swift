582b3a396a7850b39b7c8441277a9e4159c11cd3
Alexander Freas <akfreas@gmail.com>
2025-01-16 14:14:17 +0100

    Update printdebug to work statically

--- Sources/EncameraCore/Extensions/DebugPrintable.swift
+++ Sources/EncameraCore/Extensions/DebugPrintable.swift
@@ -11,10 +11,14 @@ public protocol DebugPrintable {
 }
 
 extension DebugPrintable {
-    public func printDebug(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    public static func printDebug(_ items: Any..., separator: String = " ", terminator: String = "\n") {
         let className = String(describing: type(of: self))
         let message = items.map { "\($0)" }.joined(separator: separator)
         print("\(className): \(message)", terminator: terminator)
+    }
+
+    public func printDebug(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+        Self.printDebug(items, separator: separator, terminator: terminator)
     }
 }
 
